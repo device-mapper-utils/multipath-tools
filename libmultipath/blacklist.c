ad7fa8a17f4d00c70fd18bc7db3ad3926f79fbf9
Benjamin Marzinski <bmarzins@redhat.com>
2014-07-02 12:49:53 -0500

    RH: Remove the property blacklist exception builtin
    Multipath set the default property blacklist exceptions to
    (ID_SCSI_VPD|ID_WWN).  This has the effect of blacklisting some internal
    devices.  These devices may never have multiple paths, but it is nice
    to be able to set multipath up on them all the same.  This patch simply
    removes the default, and makes it so that if no property
    blacklist_exception is given, then devices aren't failed for not matching
    it.
    
    Signed-off-by: Benjamin Marzinski <bmarzins@redhat.com>

--- libmultipath/blacklist.c
+++ libmultipath/blacklist.c
@@ -201,9 +201,6 @@ setup_default_blist (struct config * conf)
 	if (store_ble(conf->blist_devnode, "!^(sd[a-z]|dasd[a-z]|nvme[0-9])", ORIGIN_DEFAULT))
 		return 1;
 
-	if (store_ble(conf->elist_property, "(SCSI_IDENT_|ID_WWN)", ORIGIN_DEFAULT))
-		return 1;
-
 	vector_foreach_slot (conf->hwtable, hwe, i) {
 		if (hwe->bl_product) {
 			if (find_blacklist_device(conf->blist_device,
@@ -409,7 +406,8 @@ filter_property(const struct config *conf, struct udev_device *udev,
 			*uid_attribute != '\0';
 		bool uid_attr_seen = false;
 
-		r = MATCH_PROPERTY_BLIST_MISSING;
+		if (VECTOR_SIZE(conf->elist_property))
+			r = MATCH_PROPERTY_BLIST_MISSING;
 		udev_list_entry_foreach(list_entry,
 				udev_device_get_properties_list_entry(udev)) {
 
