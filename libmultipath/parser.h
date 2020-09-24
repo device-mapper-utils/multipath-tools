3a90470cd0b20f9445800fbb0a678a089b895c7e
Martin Wilck <mwilck@suse.com>
2020-09-24 15:37:07 +0200

    libmultipath: constify file argument in config parser
    Reviewed-by: Benjamin Marzinski <bmarzins@redhat.com>
    Signed-off-by: Martin Wilck <mwilck@suse.com>

--- libmultipath/parser.h
+++ libmultipath/parser.h
@@ -78,7 +78,7 @@ extern void free_keywords(vector keywords);
 extern vector alloc_strvec(char *string);
 extern void *set_value(vector strvec);
 extern void *set_regex_value(vector strvec);
-extern int process_file(struct config *conf, char *conf_file);
+extern int process_file(struct config *conf, const char *conf_file);
 extern struct keyword * find_keyword(vector keywords, vector v, char * name);
 int snprint_keyword(char *buff, int len, char *fmt, struct keyword *kw,
 		    const void *data);
