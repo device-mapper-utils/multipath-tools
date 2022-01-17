f7f98a93d57e9e56a5bd63bfc41b517b36d7b925
Benjamin Marzinski <bmarzins@redhat.com>
2022-01-17 14:45:38 -0600

    libmultipath: make helper function to trigger path uevents
    Pull the code that checks if a path needs to trigger a uevent, and
    triggers, out of trigger_paths_udev_change() and into a new function,
    trigger_path_udev_change(). This function will be used separately by
    a future patch. No functional changes.

--- libmultipath/configure.h
+++ libmultipath/configure.h
@@ -56,6 +56,7 @@ int get_refwwid (enum mpath_cmds cmd, char * dev, enum devtypes dev_type,
 		 vector pathvec, char **wwid);
 int reload_map(struct vectors *vecs, struct multipath *mpp, int refresh, int is_daemon);
 struct udev_device *get_udev_device(const char *dev, enum devtypes dev_type);
+void trigger_path_udev_change(struct path *pp, bool is_mpath);
 void trigger_paths_udev_change(struct multipath *mpp, bool is_mpath);
 void trigger_partitions_udev_change(struct udev_device *dev, const char *action,
 				    int len);
