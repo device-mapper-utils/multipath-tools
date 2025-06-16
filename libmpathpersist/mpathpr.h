2ae68aae4e62a9c171f39e23ec4883d2483905a8
Benjamin Marzinski <bmarzins@redhat.com>
2025-06-16 19:39:04 -0400

    libmpathpersist: remove uneeded wrapper function.
    mpath_send_prin_activepath() just calls prin_do_scsi_ioctl() with exactly
    the same arguments that it is passed, and returns the same return. remove
    it.

--- libmpathpersist/mpathpr.h
+++ libmpathpersist/mpathpr.h
@@ -31,7 +31,6 @@ int prin_do_scsi_ioctl(char * dev, int rq_servact, struct prin_resp * resp, int 
 int prout_do_scsi_ioctl( char * dev, int rq_servact, int rq_scope,
 		unsigned int rq_type, struct prout_param_descriptor *paramp, int noisy);
 void * _mpath_pr_update (void *arg);
-int mpath_send_prin_activepath (char * dev, int rq_servact, struct prin_resp * resp, int noisy);
 int get_mpvec (vector curmp, vector pathvec, char * refwwid);
 void * mpath_prout_pthread_fn(void *p);
 void dumpHex(const char* , int len, int no_ascii);
