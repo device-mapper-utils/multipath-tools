168d1fc199926139cc240bce4b07650bc12eaed4
Benjamin Marzinski <bmarzins@redhat.com>
2025-07-15 14:50:55 -0400

    libmpathpersist: clean up duplicate function declarations

--- multipathd/main.h
+++ multipathd/main.h
@@ -46,12 +46,6 @@ int ev_remove_map (char *, char *, int, struct vectors *);
 int flush_map(struct multipath *, struct vectors *);
 int set_config_state(enum daemon_status);
 void * mpath_alloc_prin_response(int prin_sa);
-int prin_do_scsi_ioctl(char *, int rq_servact, struct prin_resp * resp,
-		       int noisy);
-void dumpHex(const char * , int len, int no_ascii);
-int prout_do_scsi_ioctl(char * , int rq_servact, int rq_scope,
-			unsigned int rq_type,
-			struct prout_param_descriptor *param, int noisy);
 void handle_signals(bool);
 int __setup_multipath (struct vectors * vecs, struct multipath * mpp,
 		       int reset);
