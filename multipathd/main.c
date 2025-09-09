7a77a22dd0a1212ef7087268a846c19d96c8f946
Benjamin Marzinski <bmarzins@redhat.com>
2025-09-09 18:55:49 -0400

    libmpathpersist: handle updating key race condition
    If a multipath device's registered key is changed, mpathpersist needs
    to update the key in multipathd before it registers the new key on the
    paths. This means that there is a time when multipathd thinks the paths
    should be using the new key, but none of them are. If a path is
    restored after mpathpersist checks which paths are usable to set the
    key on, but before it sets the key on any path, multipathd will see
    that the new key is not registered on any paths, and think that the
    key has been preempted or cleared. This will leave the path without
    a key, and possibly make multipathd think the device does not hold
    the reservation, even if it does.
    
    To avoid this, multipathd will now remember the old key when registering
    a new one. Once the registration is finished, and (un)setprstatus is
    called, multipathd will forget the old key. Until then, multipathd will
    check for either key when it looks to see if there is an existing key.
    If the registration fails and the key get reverted, multipathd will
    also forget the old key.

--- multipathd/main.c
+++ multipathd/main.c
@@ -3700,6 +3700,7 @@ void set_pr(struct multipath *mpp)
 {
 	mpp->ever_registered_pr = true;
 	mpp->prflag = PR_SET;
+	memset(&mpp->old_pr_key, 0, 8);
 }
 
 void unset_pr(struct multipath *mpp)
@@ -3707,6 +3708,7 @@ void unset_pr(struct multipath *mpp)
 	mpp->prflag = PR_UNSET;
 	mpp->prhold = PR_UNSET;
 	mpp->sa_flags = 0;
+	memset(&mpp->old_pr_key, 0, 8);
 }
 
 /*
@@ -3755,7 +3757,15 @@ static int update_map_pr(struct multipath *mpp, struct path *pp)
 			dumpHex((char *)keyp, 8, 1);
 		}
 
-		if (!memcmp(&mpp->reservation_key, keyp, 8))
+		/*
+		 * If you are in the middle of updating a key (old_pr_key
+		 * is set) check for either the new key or the old key,
+		 * since you might be checking before any paths have
+		 * updated their keys.
+		 */
+		if (!memcmp(&mpp->reservation_key, keyp, 8) ||
+		    (get_be64(mpp->old_pr_key) &&
+		     !memcmp(&mpp->old_pr_key, keyp, 8)))
 			isFound = 1;
 	}
 
