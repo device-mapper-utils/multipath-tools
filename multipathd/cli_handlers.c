device-mapper-utils/multipath-tools

commit 1aeffa9e30efe2f683e3ea7e4528fd342bb0dfc7

libmpathpersist: handle updating key race condition
If a multipath device's registered key is changed, mpathpersist needs
to update the key in multipathd before it registers the new key on the
paths. This means that there is a time when multipathd thinks the paths
should be using the new key, but none of them are. If a path is
restored after mpathpersist checks which paths are usable to set the
key on, but before it sets the key on any path, multipathd will see
that the new key is not registered on any paths, and think that the
key has been preempted or cleared. This will leave the path without
a key, and possibly make multipathd think the device does not hold
the reservation, even if it does.

To avoid this, multipathd will now remember the old key when registering
a new one. Once the registration is finished, and (un)setprstatus is
called, multipathd will forget the old key. Until then, multipathd will
check for either key when it looks to see if there is an existing key.
If the registration fails and the key get reverted, multipathd will
also forget the old key.

Signed-off-by: Benjamin Marzinski <bmarzins@redhat.com>
Reviewed-by: Martin Wilck <mwilck@suse.com>

multipathd/main.c
@@ -4249,6 +4249,7 @@ void set_pr(struct multipath *mpp)
 {
 	mpp->ever_registered_pr = true;
 	mpp->prflag = PR_SET;
+	memset(&mpp->old_pr_key, 0, 8);
 }
 
 void unset_pr(struct multipath *mpp)
@@ -4256,6 +4257,7 @@ void unset_pr(struct multipath *mpp)
 	mpp->prflag = PR_UNSET;
 	mpp->prhold = PR_UNSET;
 	mpp->sa_flags = 0;
+	memset(&mpp->old_pr_key, 0, 8);
 }
 
 /*
@@ -4308,7 +4310,15 @@ static int update_map_pr(struct multipath *mpp, struct path *pp)
 			dumpHex((char *)keyp, 8, 1);
 		}
 
-		if (!memcmp(&mpp->reservation_key, keyp, 8))
+		/*
+		 * If you are in the middle of updating a key (old_pr_key
+		 * is set) check for either the new key or the old key,
+		 * since you might be checking before any paths have
+		 * updated their keys.
+		 */
+		if (!memcmp(&mpp->reservation_key, keyp, 8) ||
+		    (get_be64(mpp->old_pr_key) &&
+		     !memcmp(&mpp->old_pr_key, keyp, 8)))
 			isFound = 1;
 	}
 

libmultipath/prkey.c
@@ -222,10 +222,22 @@ int set_prkey(struct config *conf, struct multipath *mpp, uint64_t prkey,
 	}
 	else
 		ret = do_prkey(fd, mpp->wwid, NULL, PRKEY_WRITE);
-	if (ret == 0)
+	if (ret == 0) {
+		/*
+		 * If you are reverting back to the old key, because you
+		 * did not successfully set a new key, don't remember the
+		 * key you never successfully set.
+		 */
+		if (get_be64(mpp->old_pr_key) == prkey)
+			memset(&mpp->old_pr_key, 0, 8);
+		else
+			memcpy(&mpp->old_pr_key, &mpp->reservation_key, 8);
 		select_reservation_key(conf, mpp);
-	if (get_be64(mpp->reservation_key) != prkey)
+	}
+	if (get_be64(mpp->reservation_key) != prkey) {
+		memset(&mpp->old_pr_key, 0, 8);
 		ret = 1;
+	}
 out_file:
 	close(fd);
 out:

libmultipath/structs.h
@@ -520,6 +520,7 @@ struct multipath {
 	/* persistent management data*/
 	int prkey_source;
 	struct be64 reservation_key;
+	struct be64 old_pr_key;
 	uint8_t sa_flags;
 	int prflag;
 	int prhold;

commit b5103c7c9c1ac7f92989b3e8414ebb45c45f6b02

multipathd: Fix tracking of old PR key
When libmpathpersist is in the process of changing a registered key,
multipathd needs to remember both the old and new values of the key to
deal with a race between limpathpersist updating the key and multipathd
restoring a failed path. It was supposed to stop remembering the old
value when libmpathpersist was done changing the key and issued a
"setprstatus" command. However, clearing the old key was done in
set_pr(), which only gets called by cli_setprstatus() when registering a
new device, not when changing the key on a registered device. Also,
set_pr() is called by update_map_pr().  This means that multipathd was
forgetting the key when it shouldn't and not when it should. Fix this
to only forget the key when cli_setprstatus() is called, and always
then.

Fixes: 1aeffa9e ("libmpathpersist: handle updating key race condition")

multipathd/cli_handlers.c
@@ -1326,6 +1326,7 @@ cli_setprstatus(void * v, char ** reply, int * len, void * data)
 		set_pr(mpp);
 		condlog(2, "%s: prflag set", param);
 	}
+	memset(&mpp->old_pr_key, 0, 8);
 
 
 	return 0;

multipathd/main.c
@@ -3678,7 +3678,6 @@ void set_pr(struct multipath *mpp)
 {
 	mpp->ever_registered_pr = true;
 	mpp->prflag = PR_SET;
-	memset(&mpp->old_pr_key, 0, 8);
 }
 
 void unset_pr(struct multipath *mpp)